3bfb13ea4af631137b01227a8571bc0d31134679
Dennis ngugi wambui <112067611+dennisngugiwambui@users.noreply.github.com>
2024-01-29 13:56:56 -0800

    Add files via upload
    farmer model

---
+++ app/Models/Farmer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Farmer extends Model
+{
+    use HasFactory;
+
+    use protected $fillable =
+    [
+        'full_name',
+        'national_id',
+        'phone',
+        'email',
+        'location',
+        'crop_type',
+        'farming_experience',
+        'farming_type',
+        'gender',
+        'farmSize',
+        'cropsGrown',
+    ];
+}
